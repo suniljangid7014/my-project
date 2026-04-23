328525b6cdb12b3abce09e050b0b6060dfcb25fe
Sunil Kumar <jangidsunil7014@gmail.com>
2026-04-23 17:39:58 +0530

    Initial commit

---
+++ src/com/infy/ClassWithSecrets.java
@@ -0,0 +1,13 @@
+package com.infy;
+
+public class ClassWithSecrets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String username = "USERNAME";
+		String password = "Password";
+		System.out.println(username+" "+password);
+
+	}
+
+}
